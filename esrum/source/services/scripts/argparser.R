70ae959cc7df084b48f85f2d67e797ff19007d26
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2024-04-17 13:51:46 +0200

    expand R/RStudio section

---
+++ esrum/source/services/scripts/argparser.R
@@ -0,0 +1,11 @@
+
+   #!/usr/bin/env Rscript
+   library(argparser)
+
+   parser <- arg_parser("This is my script!")
+
+   parser <- add_argument(parser, "input_file", help="My data")
+   parser <- add_argument(parser, "--p-value", default=0.05, help="Maximum P-value")
+
+   args <- parse_args(parser)
+   cat("I would process the file", args$input_file, "with a max P-value of", args$p_value, "\n")
